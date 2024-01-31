513f4e0c42cffa2f5d3b35df23a180b9e122ea54
Antonio Bandeira <104794351+antonioviniciusvb@users.noreply.github.com>
2024-01-31 09:56:22 -0300

    :sparkles: Criação crud-categoria

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias'
+import FormularioCategoria from './components/categorias/formularioCategoria/FormularioCategoria'
+import DeletarCategoria from './components/categorias/deletarCategoria/DeletarCategoria'
 
 function App() {
 
@@ -11,18 +14,18 @@ function App() {
   return (
     <>
       <BrowserRouter>
-      <Navbar/>
-      <div className='min-h-[75vh]'>
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          {/* <Route path="/categorias" element={<ListaCategorias /> }/>
-              <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
-              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
-              <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} /> */}
-        </Routes>
+        <Navbar />
+        <div className='min-h-[75vh]'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/categorias" element={<ListaCategorias />} />
+            <Route path="/cadastroCategoria" element={<FormularioCategoria />} />
+            <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+            <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
+          </Routes>
         </div>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </>
 
